5a69161dafd70f05d01941fdcdcc519dd4e60be0
lili2628 <113315152+lili2628@users.noreply.github.com>
2022-12-15 16:07:28 +0000

    Deploying to gh-pages from @ lili2628/dream-team-15@05a3445987010e2ed5a3df8d7945f77b4d3fa284 🚀

---
+++ index.3b45770f.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,speed:1e3,easing:"linear",autoplaySpeed:3e3,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0})}));
+//# sourceMappingURL=index.3b45770f.js.map
---
+++ index.58e1f119.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".slider").slick({arrows:!1,dots:!0,speed:1e3,easing:"linear",autoplaySpeed:3e3,pauseOnFocus:!0,pauseOnHover:!0,pauseOnDotsHover:!0})}));
+//# sourceMappingURL=index.58e1f119.js.map
